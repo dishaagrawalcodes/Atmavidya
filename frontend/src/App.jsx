b3c711a6b54ef5a499212f769bf936f01bf289c9
dishaagrawalcodes <dishaagrawal9568@gmail.com>
2025-05-08 23:41:23 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Featured from "./components/Featured";
-import About from "./components/About";
 
 const App = () => {
   return (
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,31 +1,31 @@
-import React from "react";
+import React from 'react'
 
-const Footer = () => {
+function Footer() {
   return (
     <>
     
 
-<footer class="w-full p-4 mt-10 md:flex md:items-center md:justify-between md:p-6 bg-gray-800 ">
-    <span class="text-sm text-gray-300 sm:text-center ">© 2023 <a href="#" class="hover:underline">RecursiveMinds™</a>. All Rights Reserved.
+<footer className="w-full p-4  md:flex md:items-center md:justify-between md:p-6 bg-[#33160a] ">
+    <span className="text-sm text-gray-300 sm:text-center ">© 2023 <a href="#" className="hover:underline">RecursiveMinds™</a>. All Rights Reserved.
     </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-300 sm:mt-0">
+    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-300 sm:mt-0">
         <li>
-            <a href="#" class="hover:underline me-4 md:me-6">About</a>
+            <a href="#" className="hover:underline me-4 md:me-6">About</a>
         </li>
         <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+            <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
         </li>
         <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+            <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
         </li>
         <li>
-            <a href="#" class="hover:underline">Contact</a>
+            <a href="#" className="hover:underline">Contact</a>
         </li>
     </ul>
 </footer>
 
     </>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
