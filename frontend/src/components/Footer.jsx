e15fe40da209974e68a642bec8ac6d869b05e13a
Disha Agrawal <166953658+dishaagrawalcodes@users.noreply.github.com>
2025-05-13 21:24:28 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-300 sm:mt-0">
         <li>
-            <a href="#" className="hover:underline me-4 md:me-6">About</a>
+            <a href="#" className="hover:underline me-4 md:me-6"> ABOUt</a>
         </li>
         <li>
             <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
